01459ba92a034934d2cf97542f795af951e4396e
tatusovich <usovichtat@gmail.com>
2024-06-11 15:15:20 +0300

    Add files via upload

---
+++ homework3-1.py
@@ -0,0 +1,9 @@
+name ='Mark'
+print('name', ":" , name )
+age = 7
+print('age',':', age)
+neu_age = 10
+age = neu_age + age
+print('neu_age', ':', age)
+is_student = True
+print('is_student', ':',is_student)
---
+++ homework3.py
@@ -0,0 +1,12 @@
+first_name ='всего задач'
+home_count = 12
+second_name = 'затрачено часов'
+time_cont= 1.5
+time_m =time_cont /home_count
+print(time_m)
+course='Pyhton'
+print('Курс:Pyhton ,',first_name,':',home_count, ",", second_name,':',time_cont, ",",
+'среднее время выполнения', '0,125')
+
+
+
